6e2b6d04814c4db0fb89cafe3655667b409eb01b
sisiz <zimmerman.sisi@gmail.com>
2014-05-28 13:53:24 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -5,10 +5,25 @@ public class Board{
       
       public Board(int characternumber){
           characterLocations = new BoardNode[characternumber];
-          start = new BoardNode(-1, -1, -1, "GO", null;);
+          _start = new BoardNode(-1, -1, -1, "GO", null);
+          BoardNode temp1 = new BoardNode(7, 50, 450, "loc last", _start);
+          BoardNode temp1 = new BoardNode(7, 50, 450, "loc last", _start);
+          BoardNode temp1 = new BoardNode(7, 50, 450, "loc last", _start);
+          BoardNode temp1 = new BoardNode(7, 50, 450, "loc last", _start);
+          BoardNode temp1 = new BoardNode(7, 50, 450, "loc last", _start);
+          BoardNode temp1 = new BoardNode(7, 50, 450, "loc last", _start);
           //add the rest of the locations in
         
+        
+        
+        //stat.setNext(last)
       }
+  
+      private void add(BoardNode newNode){
+            
+            
+      }
+  
   
       public void 
 
