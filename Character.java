b0e99ae399b72e29ce0390d28d832bcaf156ffb0
sisiz <zimmerman.sisi@gmail.com>
2014-05-28 19:27:08 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -2,26 +2,48 @@ public class Character{
 
     String name;
     int money;
+    int number
     ArrayList<BoardNode> properties;
-    BoardNode CurrentLocation;
+    BoardNode currentLocation;
     Board board;
 
     public Character(String n, Board b){
             name = n;
             money = 1500;
             properties = null;
-            Board = b;
-            CurrentLocation = b.start();
+            board = b;
+            currentLocation = b.start();
     }
 
     public int roll(){
-        return (int)(Math.random() *6) + 1;
+        int x = (int)(Math.random() *6) +  (int)(Math.random() *6) + 2; 
+        System.out.println("you rolled a: " + x);
+        return x;
+        //maybe addin something for doubles later?
     }
 
     public void buyProperty( BoardNode location );
 
     public void payRent(BoardNode location);
 
-    
+    public void move(){
+        int x = roll()
+        for (int i = 0; i< x; i++){
+            currentLocation = currentLocation.getNext();
+        }
+        if (currentLocation.canPurchase()){
+            if(currentLocation.getPrice() < money){
+                    //ask if they want to buy
+                    //if yer add to properties
+            }else{
+                System.out.println("you do not have enough movey to purchase this location");
+            }
+        }else{
+            //if has owner pay rent
+                //if can't pay rent lose
+            //if non-purchusable thing do that action
+            
+        }
+    }
 
 }
