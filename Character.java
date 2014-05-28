7865e78d532c44313796f1bb93c9f0bb35e48303
sisiz <zimmerman.sisi@gmail.com>
2014-05-28 13:40:06 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,6 +1,22 @@
-public interface Character{
+public class Character{
 
-    public int roll();
+    String name;
+    int money;
+    ArrayList<BoardNode> properties;
+    BoardNode CurrentLocation;
+    Board board;
+
+    public Character(String n, Board b){
+            name = n;
+            money = 1500;
+            properties = null;
+            Board = b;
+            CurrentLocation = b.start();
+    }
+
+    public int roll(){
+        return (int)(Math.random() *6) + 1;
+    }
 
     public void buyProperty( BoardNode location );
 
