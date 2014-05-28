5495037a11b60e706a66a312ddebf9f3b5fd4503
sisiz <zimmerman.sisi@gmail.com>
2014-05-28 13:44:30 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,7 +1,16 @@
 public class Board{
+      
+      BoardNode _start;
+      BoardNode[] charterLocations;
+      
+      public Board(int characternumber){
+          characterLocations = new BoardNode[characternumber];
+          start = new BoardNode("Go");
+          //add the rest of the locations in
+        
+      }
   
-
-
+      public void 
 
 
 
