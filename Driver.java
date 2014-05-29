051dca434d5052c86e4cb852c1a7ad055f4d5711
sisiz <zimmerman.sisi@gmail.com>
2014-05-29 13:59:53 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class Driver{
 
 
@@ -11,9 +13,27 @@ public class Driver{
 
 
 
+  public static void main( String[] args ){
+      Board _board = new Board(2);
+    
+    
+    Scanner scan = new Scanner()
+    Player _player1;
+    Player _player2;
+          //later ask how many players? add compter players?
+    System.out.println("what is the name of the first palyer?");
+    String name = scan.nextLine();
+    _player1.setName(name);
+    
+    System.out.println("what is the name of the second palyer?");
+    name = scan.nextLine();
+    _player2.setName(name);
+    
 
 
 
 
+
+  }
 
 }
