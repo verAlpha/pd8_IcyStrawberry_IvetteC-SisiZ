4f6a59658f3c8d8fedc3eb1c0cda7be4932822ac
sisiz <zimmerman.sisi@gmail.com>
2014-05-30 13:45:02 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -14,12 +14,12 @@ public class Driver{
 
 
   public static void main( String[] args ){
-      Board _board = new Board(2);
-    
+    Board _board = new Board(2);
+    boolean end = false;//determines if the game is over
     
     Scanner scan = new Scanner()
-    Player _player1;
-    Player _player2;
+    Player _player1 = new Player("",_board);
+    Player _player2 = new Player("",_board);
           //later ask how many players? add compter players?
     System.out.println("What is the name of the first palyer?");
     String name = scan.nextLine();
@@ -36,27 +36,17 @@ public class Driver{
     System.out.println("Roll to determine the turn order. Please type roll");
     int dice2 = _player2.roll();
 
-    if (dice1 > dice2){
-      _player1.move();
-      //System.out.println("Please type one of the following: buy property, pass, bank and property information, ");
-      // we have to write a pass method and a bank and property information grabbing method.
-     /* String response = scan.nextLine();
-      if (response.equals("buy property"))
-        _player1.buyProperty();
-      else
-        // implement code*/
-    }
-    // ahould we deal with the case that if they're both equal?
-    else{
-      _player2.move();
-     /* System.out.println("Please type one of the following: buy property, pass, bank and property information, ");
-      // we have to write a pass method and a bank and property information grabbing method.
-      String response = scan.nextLine();
-      if (response.equals("buy property"))
-        _player2.buyProperty();
-      else*/
-        // implement code
-    }
-  }
+    while(!end){
+       // how would we deal with the case that if they're both equal? reroll?
+      if (dice1 > dice2){
+        _player1.move();
+        _player2.mover();
+
+      }else{
+        _player2.move();
+        _player1.move();
+      }
+    } 
+      
 
 }
