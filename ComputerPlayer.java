7907df481dba7bd46535449ec8aeb3f634eb2374
verAlpha <Ivette.chen.21@gmail.com>
2014-06-10 06:23:08 -0400

    Worked on Computer but didn't do that much. sorry

---
+++ ComputerPlayer.java
@@ -0,0 +1,19 @@
+public class ComputerPlayer{
+
+    public Character(String n, Board b){
+	super(n, b);
+	String[] comp = {"Bob", "Sam", "John", "Mary"};
+	name = comp[(int)(Math.random() * comp.length)];
+    }
+
+    
+
+    public boolean buyProperty(BoardNode location){
+	if (Math.random() > .2)
+	    super();
+	else
+	    return false; //passes it.
+    }
+}
+
+    
