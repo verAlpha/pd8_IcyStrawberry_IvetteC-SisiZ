74456d762647237464b44c555bfa167c6fe18af3
sisiz <zimmerman.sisi@gmail.com>
2014-06-14 16:35:52 -0400

    Create ComputerPlayer.java
    i couldn't get it to merge b/c i edited it here then my computer so i just deleted file and copy and pasted it back in as new file

---
+++ ComputerPlayer.java
@@ -0,0 +1,52 @@
+public class ComputerPlayer extends Character{
+
+    private String[] NAMES = {"Bob", "Sam", "John", "Mary"};
+
+    public ComputerPlayer( String s, Board b){
+	super(s, b);	
+	String n = NAMES[(int)(Math.random() * NAMES.length)];
+	name = n;
+    }
+
+    public boolean buyProperty(BoardNode location){
+	if (Math.random() > .2){
+	    return super.buyProperty(location);
+	}else{
+	    return false; //passes it.
+	}
+    }	
+    
+
+    //maybe add ability to buy houses
+   
+
+    public void buyHouse(){
+	if (Math.random() > .2){//randomly chooses to buy house
+	    BoardNode prop = null;
+	    for(int i = 0; i < properties.length; i++){
+		if(properties[i].getFullSet()){
+		    prop = properties[i];
+		    break;//just chooses first property that is full set
+		}
+	    }	    
+	    if(prop != null){//if  any full sets
+		if ((prop.getColor()/4) * 50 < money/2){//if can afford
+		    prop.addHouse();
+		    money -= prop.getColor()/4 * 50;
+		    System.out.println(name + "has bought a house for"+
+				       prop.getName());
+		}
+	    }
+	}
+    }
+
+    public boolean move(){
+	moveHelp();
+	buyHouse();
+		
+	return endConditions();
+    }
+
+
+}
+    
